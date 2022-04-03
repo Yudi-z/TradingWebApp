1d043ad1777c91fef073b1ea90aa035043c39541
Yudi Zhang <zhan5155@umn.edu>
2022-04-03 02:39:00 -0400

    getStock unit test passed

--- src/main/java/com/ibm/security/appscan/altoromutual/util/yahooUtil.java
+++ src/main/java/com/ibm/security/appscan/altoromutual/util/yahooUtil.java
@@ -6,6 +6,8 @@ import yahoofinance.histquotes.HistoricalQuote;
 import yahoofinance.histquotes.Interval;
 
 import java.io.IOException;
+import java.math.BigDecimal;
+import java.sql.Timestamp;
 import java.util.Calendar;
 import java.util.List;
 
@@ -23,14 +25,26 @@ public class yahooUtil {
     public static void getStock(String Ticker){
         Calendar from = Calendar.getInstance();
         Calendar to = Calendar.getInstance();
-        from.add(Calendar.YEAR, -5); // from 5 years ago
+        from.add(Calendar.YEAR, -1); // from 5 years ago
 
         try {
             Stock stock = YahooFinance.get(Ticker);
-            List<HistoricalQuote> googleHistQuotes = stock.getHistory(from, to, Interval.DAILY);
-//            System.out.println(stock.getQuote());
+            List<HistoricalQuote> stockHistQuotes = stock.getHistory(from, to, Interval.MONTHLY);
+            for (int i=0;i<stockHistQuotes.size();i++) {
+                Double adjClose = stockHistQuotes.get(i).getAdjClose().doubleValue();
+                Timestamp time = new Timestamp(stockHistQuotes.get(i).getDate().getTimeInMillis());
+                String ret = "Time: "+time + ", Stock: "+ Ticker + ", Adjusted close price: "+ adjClose;
+                System.out.println(ret);
+            }
+
         } catch (IOException e) {
             e.printStackTrace();
         }
+    }
+
+    public static void main(String[] args) {
+        getStock("GOOG");
+//        yahooUtil.getGOOG();
+//    System.out.println(google);
     }
 }
