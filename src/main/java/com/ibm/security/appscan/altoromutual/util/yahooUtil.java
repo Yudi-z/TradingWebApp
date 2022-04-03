a164e61055312beb57b8afd1160e503e826d40cd
Yudi Zhang <zhan5155@umn.edu>
2022-04-03 02:26:53 -0400

    getStock with interval

--- src/main/java/com/ibm/security/appscan/altoromutual/util/yahooUtil.java
+++ src/main/java/com/ibm/security/appscan/altoromutual/util/yahooUtil.java
@@ -2,14 +2,33 @@ package com.ibm.security.appscan.altoromutual.util;
 
 import yahoofinance.Stock;
 import yahoofinance.YahooFinance;
+import yahoofinance.histquotes.HistoricalQuote;
+import yahoofinance.histquotes.Interval;
 
 import java.io.IOException;
+import java.util.Calendar;
+import java.util.List;
 
 public class yahooUtil {
+
     public static void getGOOG(){
         try {
             Stock google = YahooFinance.get("GOOG");
             System.out.println(google.getQuote());
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+    }
+
+    public static void getStock(String Ticker){
+        Calendar from = Calendar.getInstance();
+        Calendar to = Calendar.getInstance();
+        from.add(Calendar.YEAR, -5); // from 5 years ago
+
+        try {
+            Stock stock = YahooFinance.get(Ticker);
+            List<HistoricalQuote> googleHistQuotes = stock.getHistory(from, to, Interval.DAILY);
+//            System.out.println(stock.getQuote());
         } catch (IOException e) {
             e.printStackTrace();
         }
