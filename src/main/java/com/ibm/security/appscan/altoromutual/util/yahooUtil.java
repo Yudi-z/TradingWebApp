e582596964fe33448212c0b98b6c729b7b5ee4df
zp50 <zpeng5@u.rochester.edu>
2022-04-10 19:12:19 -0400

    Add "getStockReturns","getStockPrices","getDates"

--- src/main/java/com/ibm/security/appscan/altoromutual/util/yahooUtil.java
+++ src/main/java/com/ibm/security/appscan/altoromutual/util/yahooUtil.java
@@ -47,6 +47,18 @@ public class yahooUtil {
         }
         return prices;
     }
+    public static ArrayList<Double>getReturns(String Ticker){
+        ArrayList<Double> daily_yield = new ArrayList<>();
+        ArrayList<Double> prices = getStockPrice(Ticker);
+        daily_yield.add((double) 0);
+        for (int i = 1; i < prices.size(); i++) {
+            double this_close = prices.get(i);
+            double prev_close = prices.get(i - 1);
+            double yield = (this_close - prev_close) / prev_close;
+            daily_yield.add(yield);
+        }
+        return daily_yield;
+    }
 
     public static ArrayList<String>getDate(String Ticker){
         ArrayList<String> dates=new ArrayList<>();
@@ -73,7 +85,7 @@ public class yahooUtil {
             Timestamp time = new Timestamp(stockHistQuotes.get(i).getDate().getTimeInMillis());
             String ret = "Time: "+time + ", Stock: "+ "GOOG" + ", Adjusted close price: "+ adjClose;
             System.out.println(ret);
-           // System.out.println(getDate("GOOG").get(i));
+           //System.out.println(getReturns("GOOG").get(i));
         }
 //        yahooUtil.getGOOG();
 //    System.out.println(google);
